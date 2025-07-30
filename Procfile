66072947943188d06fe8321e9ce1985a094e6860
Eliobros <eliobrostech3@gmail.com>
2025-07-30 04:28:59 +0000

    atualizando a api

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python tina.py
--- tina.py
+++ tina.py
@@ -209,5 +209,6 @@ def send_message(recipient_id, message_text):
     return response.json()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
